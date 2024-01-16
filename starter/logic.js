c401cc62125761f186a7dc3c79d1f70f7113acac
fj010101 <fozia.jaylani@hotmail.com>
2024-01-16 23:35:20 +0000

    Rename logic.js and questions.js to starter/logic.js and starter/questions.js

--- starter/logic.js
+++ starter/logic.js
@@ -1,8 +1,49 @@
-// logic js
-
 const questions = [
+// questions.js
 
-];
+    {
+        question: "What does 'JS' stand for?",
+        choices: ["Java Source", "JavaScript", "Just Saying", "Jungle Sound]"],
+        correctIndex: 1,
+    },
+    {
+        question: "Which operator is used for concatenation in JavaScript?",
+        choices: ["+", "*", "&", "^"],
+        correctIndex: 0,
+    
+    }, 
+    {
+        question: "What is the purpose of the 'if' statement in JavaScript?",
+        choices: ["Looping", "Conditionally execute code", "Defining a function", "Creating an array"],
+        correctIndex: 1,
+    },
+    {
+    
+        question: "What function is used to print content to the console in JavaScript?",
+        choices: ["print()", "log()", "display()", "console()"],
+        correctIndex: 1,
+    },
+            {
+        question: "Which symbol is used for single-line comments in JavaScript?",
+        choices: ["//", "/*", "#", "--"],
+        correctIndex: 0,
+    },
+    {
+    
+        question: "What method is used to convert a string to an integer in JavaScript?",
+        choices: ["toInt()", "parse()", "convertToInt()", "parentInt()"],
+        correctIndex: 3,
+    
+    },
+            {
+        question: "In JavaScript, what type of data is 'true'?",
+        choices: ["Boolean", "String", "Number", "Object"],
+            },
+     
+        // End of questions.js
+    
+    ];
+    
 
 const startButton = document.getElementById('start');
 const questionsContainer = document.getElementById('questions');
