cd54e2180de92791eb2a754c6ee5d2926918710f
fj010101 <fozia.jaylani@hotmail.com>
2024-01-16 21:24:59 +0000

    Modify questions.js file

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,26 @@
+// questions.js
+
+    const questions = [
+
+{
+    question: "What does 'JS' stand for?",
+    choices: ["Java Source", "JavaScript", "Just Saying", "Jungle Sound]"],
+    correctIndex: 1,
+},
+{
+    question: "Which operator is used for concatenation in JavaScript?",
+    choices: ["+", "*", "&", "^"],
+    correctIndex: 0,
+
+},
+{
+    question: "What is the purpose of the 'if' statement in JavaScript?",
+    choices: ["Looping", "Conditionally execute code", "Defining a function", "Creating an array"],
+    correctIndex: 1,
+},
+{
+    
+
+    // End of questions.js
+}
+];
