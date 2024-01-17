56af8e87b57eebf7dc95fa052b08c2a9e1935e0d
fj010101 <151868892+fj010101@users.noreply.github.com>
2024-01-17 01:44:13 +0000

    Update logic.js

--- starter/logic.js
+++ starter/logic.js
@@ -1,53 +1,4 @@
-export const questions = [
-    
-// questions.js
 
-    {
-        question: "What does 'JS' stand for?",
-        choices: ["Java Source", "JavaScript", "Just Saying", "Jungle Sound]"],
-        correctIndex: 1,
-    },
-    {
-        question: "Which operator is used for concatenation in JavaScript?",
-        choices: ["+", "*", "&", "^"],
-        correctIndex: 0,
-    
-    }, 
-    {
-        question: "What is the purpose of the 'if' statement in JavaScript?",
-        choices: ["Looping", "Conditionally execute code", "Defining a function", "Creating an array"],
-        correctIndex: 1,
-    },
-    {
-    
-        question: "What function is used to print content to the console in JavaScript?",
-        choices: ["print()", "log()", "display()", "console()"],
-        correctIndex: 1,
-    },
-            {
-        question: "Which symbol is used for single-line comments in JavaScript?",
-        choices: ["//", "/*", "#", "--"],
-        correctIndex: 0,
-    },
-    {
-    
-        question: "What method is used to convert a string to an integer in JavaScript?",
-        choices: ["toInt()", "parse()", "convertToInt()", "parentInt()"],
-        correctIndex: 3,
-    
-    },
-            {
-        question: "In JavaScript, what type of data is 'true'?",
-        choices: ["Boolean", "String", "Number", "Object"],
-            },
-     
-        // End of questions.js
-    
-    ];
-    
-
-=======
->>>>>>> 8f619e967cb8969ef85e323ddcaac50cc30e7a46
 const startButton = document.getElementById('start');
 const questionsContainer = document.getElementById('questions');
 const choicesContainer = document.getElementById('choices');
@@ -64,7 +15,6 @@ let score = 0;
 
 console.log('startButton:', startButton);
 startButton.addEventListener('click', startQuiz);
-submitButton.addEventListener('click', saveScore);
 function startQuiz() {
     console.log("Start Quiz button clicked!");
 }
@@ -128,6 +78,10 @@ function updateTimer() {
     }
 }
 
+
 function saveScore() {
+    function saveScore() {
+        console.log('Save Score function called');
     const initials = initialsInput.ariaValueMax;
+}
 }
